793cd87ee6ab97a7be0efbc89f0b99d9246a8982
Noushad <noushadozi333@gmail.com>
2025-01-23 17:39:44 +0600

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
       <section className="z-0 min-h-screen bg-blue-500">
