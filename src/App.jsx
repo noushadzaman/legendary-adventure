3c76ff2118cac74de22d4a39c93093b57d5e0a1a
Noushad <noushadozi333@gmail.com>
2025-01-24 23:43:48 +0600

    story

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About"
 import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Story from "./components/Story"
 
 const App = () => {
   return (
@@ -10,9 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
-      <section className="z-0 min-h-screen bg-blue-500">
-
-      </section>
+      <Story />
     </main>
   )
 }
---
+++ src/components/Story.jsx
@@ -0,0 +1,96 @@
+import { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle"
+import gsap from "gsap";
+import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
+
+const Story = () => {
+    const frameRef = useRef('null');
+
+    const handleMouseLeave = () => {
+        const element = frameRef.current;
+
+        gsap.to(element, {
+            duration: 0.3,
+            rotateX: 0,
+            rotateY: 0,
+            ease: 'power1.inOut'
+        })
+
+    }
+    const handleMouseMove = (e) => {
+        const { clientX, clientY } = e;
+        const element = frameRef.current;
+
+        if (!element) return;
+
+        const rect = element.getBoundingClientRect();
+        const x = clientX - rect.left;
+        const y = clientX - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * -10;
+        const rotateY = ((y - centerX) / centerY) * 10;
+
+        gsap.to(element, {
+            duration: 0.3,
+            rotateX,
+            rotateY,
+            transformPerspective: 500,
+            ease: 'power1.inOut'
+        })
+    }
+    const handleMouseUp = () => {
+
+    }
+    const handleMouseEnter = () => {
+
+    }
+
+    return (
+        <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+            <div className="flex size-full flex-col items-center py-10 pb-24">
+                <p className="font-general text-sm uppercase">
+                    The multiverse ip world
+                </p>
+                <div className="relative size-full">
+                    <AnimatedTitle
+                        title="The st<b>o</b>ry of <b/> a hidden real<b>m</b>."
+                        sectionId="#story"
+                        containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+                    />
+
+
+                    <div className="story-img-container">
+                        <div className="story-img-mask">
+                            <div className="story-img-content">
+                                <img
+                                    ref={frameRef}
+                                    onMouseLeave={handleMouseLeave}
+                                    onMouseUp={handleMouseUp}
+                                    onMouseEnter={handleMouseEnter}
+                                    onMouseMove={handleMouseMove}
+                                    src="/img/entrance.webp"
+                                    alt="entrance"
+                                    className="object-contain"
+                                />
+                            </div>
+                        </div>
+                        <RoundedCorners />
+                    </div>
+                </div>
+
+                <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44  md:justify-end">
+                    <div className="flex h-full w-fit flex-col items-center md:items-start">
+                        <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">Where realms coverage, lies zentry and the boundless pillar. Discover its secrets and shape your fate amidst infintie opportunities.</p>
+                        <Button id="realm-button" title="discover prologue" containerClass="mt-5" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Story
